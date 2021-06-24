8622b986a6a153349cd2ddb3d016c721cebdc261
Jamshid <mamatovjamshid506@gmail.com>
2021-06-24 09:34:00 +0500

    task add function inserted

--- AddTask_feature/views.py
+++ AddTask_feature/views.py
@@ -1,7 +1,35 @@
 from django.shortcuts import render
-from django.http import HttpResponse,JsonResponse
+from django.http import JsonResponse
+from tinydb import TinyDB,Query
+import json
+
+db=TinyDB('database.json')
+
+Tasks=Query()
+
+data={}
+
+# db.truncate()
 
 def addTask(request):
+    # print(request.method)  
     
-    return JsonResponse({"a":1,'c':[{'b':2},{'c':3}]})
+    # print(number)
+    
+    if request.method=="GET":
+
+        task_name=request.GET.get('task')
+        
+        db.insert({'task':task_name})
+        number=len(db.all())
+        print(number)
+        item=db.search(Tasks.task==task_name)
+        
+        
+        data[number]=item[-1]
+
+    # print(data)
+
+    # print(task_name)
+    return JsonResponse(data)
 
--- my_list/urls.py
+++ my_list/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from django.urls.conf import include
 from tinydb import TinyDB
 db=TinyDB('database.json')
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("addtask/",include("AddTask_feature.urls"))

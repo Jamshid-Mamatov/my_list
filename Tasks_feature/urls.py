97875bc484c3d10a957fd39713397e954e6b14f9
Jamshid <mamatovjamshid506@gmail.com>
2021-06-24 22:20:43 +0500

    added update function      work in progress...

--- Tasks_feature/urls.py
+++ Tasks_feature/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns=[
     path('',views.addTask),
-    path("del/",views.delTask)
+    path("del/",views.delTask),
+    path("update/",views.updatetask),
 ]
--- Tasks_feature/views.py
+++ Tasks_feature/views.py
@@ -19,7 +19,8 @@ def addTask(request):
 
         task_name=request.GET.get('task')
         if task_name!=None:
-            db.insert({'task':task_name})
+            lst=db.all()
+            db.insert({'task'+str(len(lst)+1):task_name})
             # number=len(db.all())
             # print(number)
             lst=db.all()
@@ -52,3 +53,17 @@ def delTask(request):
             
     return JsonResponse(data)
 
+
+def updatetask(request):
+    data={}
+    
+    task_name=request.GET.get()
+    print(task_name)
+    db.update({"task":task_name},Tasks.task.exists())
+    lst=db.all()
+    print(lst)
+    for ind,val in enumerate(lst):
+            
+        data[str(ind+1)+"-task"]=val
+
+    return JsonResponse(data)
